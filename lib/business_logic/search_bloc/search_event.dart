b18eeb98abe3e3397dee92cb73b9a956de06b350
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-20 00:04:15 +0600

    created search event classes

---
+++ lib/business_logic/search_bloc/search_event.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class SearchProductEvent extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+
+class SearchEvent extends SearchProductEvent{}
+
+class DataLoadedEvent extends SearchProductEvent{}
+
+
