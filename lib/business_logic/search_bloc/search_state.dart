a491e95d5341fb9fba0956623c90aeb55016ace2
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-20 00:08:11 +0600

    created search state classes

--- lib/business_logic/search_bloc/search_state.dart
+++ lib/business_logic/search_bloc/search_state.dart
@@ -1,1 +1,19 @@
+import 'package:equatable/equatable.dart';
+import 'package:mi_commerce/business_logic/search_bloc/search_event.dart';
+import 'package:mi_commerce/data/models/products_model.dart';
 
+class SearchProductState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class LoadingState extends SearchProductState {}
+
+class LoadedState extends SearchProductEvent {
+  final ProductsModel productsModel;
+
+  LoadedState(this.productsModel);
+}
+
+class OnErrorState extends SearchProductState{}
