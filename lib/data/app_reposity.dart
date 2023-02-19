71622d62f74c9a376bb49184ab7f2bfb5fe4d33f
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-19 23:38:59 +0600

    get product data from getSearchResult method

--- lib/data/app_reposity.dart
+++ lib/data/app_reposity.dart
@@ -1,15 +1,25 @@
 import 'package:mi_commerce/data/connection_helper.dart';
+import 'package:mi_commerce/data/models/products_model.dart';
 
 class AppRepository {
   final baseUrl = "https://panel.supplyline.network/api";
   final ConnectionHelper _connectionHelper = ConnectionHelper();
 
-  _getSearchResult({searchPattern, offsetLimit = 10}) async {
+  Future<ProductsModel?> getSearchResult(
+      {searchPattern, offsetLimit = 10}) async {
     try {
       final response = await _connectionHelper.getData(
           "$baseUrl//search-suggestions/?limit=10&offset=$offsetLimit&search=$searchPattern");
+
+      if (response != null) {
+        if (response.statusCode == 200) {
+          return ProductsModel.fromJson(response.data);
+        }
+      }
+      return null;
     } catch (e) {
       Exception(e);
+      return null;
     }
   }
 }
