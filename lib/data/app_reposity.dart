68b3d9da1ca16221422f3bb79d8ea0fd5433f9bd
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-19 23:27:45 +0600

    created getSearchResult api method to get data

--- lib/data/app_reposity.dart
+++ lib/data/app_reposity.dart
@@ -1,3 +1,15 @@
-class AppRepository{
-  
+import 'package:mi_commerce/data/connection_helper.dart';
+
+class AppRepository {
+  final baseUrl = "https://panel.supplyline.network/api";
+  final ConnectionHelper _connectionHelper = ConnectionHelper();
+
+  _getSearchResult({searchPattern, offsetLimit = 10}) async {
+    try {
+      final response = await _connectionHelper.getData(
+          "$baseUrl//search-suggestions/?limit=10&offset=$offsetLimit&search=$searchPattern");
+    } catch (e) {
+      Exception(e);
+    }
+  }
 }
