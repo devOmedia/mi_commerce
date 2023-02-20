add73812878706a85eb5e36a1eeb5bfb923eb028
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-20 21:28:31 +0600

    add bloc consumer at srach screen

--- lib/presentation/screens/search_screen.dart
+++ lib/presentation/screens/search_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mi_commerce/business_logic/search_bloc/search_bloc.dart';
+import 'package:mi_commerce/business_logic/search_bloc/search_state.dart';
 
 import '../widgets/custom_search_field.dart';
 
@@ -28,17 +31,24 @@ class _SearchScreenState extends State<SearchScreen> {
     final Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                //search field
-                CustomSearchFieldWidget(
-                    size: size, searchController: _searchController),
-              ],
-            ),
-          ),
+        body: BlocConsumer<SearchProductBloc, SearchProductState>(
+          listener: (context, state) {
+            
+          },
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    //search field
+                    CustomSearchFieldWidget(
+                        size: size, searchController: _searchController),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
