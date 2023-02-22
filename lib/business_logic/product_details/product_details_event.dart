747d919dd986b555bedc51e9289616cf7c1618d3
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-22 08:56:16 +0600

    product details event classes are created

---
+++ lib/business_logic/product_details/product_details_event.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class ProductDetailsEvent extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class GetProductDetails extends ProductDetailsEvent {
+  final String slug;
+  GetProductDetails({required this.slug});
+}
