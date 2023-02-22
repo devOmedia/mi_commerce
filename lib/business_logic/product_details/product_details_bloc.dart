7663c1f5a26ba5b54be96f9e296825b1c4a5b50f
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-22 09:07:21 +0600

    created GetProductDetails event bloc hander method

--- lib/business_logic/product_details/product_details_bloc.dart
+++ lib/business_logic/product_details/product_details_bloc.dart
@@ -5,7 +5,20 @@ import 'package:mi_commerce/data/app_reposity.dart';
 
 class ProductDetailsBloc
     extends Bloc<ProductDetailsEvent, ProductDetailsState> {
-  ProductDetailsBloc({required this.repository}) : super(ProductDetailsState()) {}
+  ProductDetailsBloc({required this.repository})
+      : super(ProductDetailsState()) {
+    on<GetProductDetails>((event, emit) async {
+      emit(ProductDetailsLoadingState());
+
+      final details = repository.getProductDetails(event.slug);
+
+      if (details != null) {
+        emit(ProductDetailsLoadedState(detailsData: details));
+      } else {
+        emit(ProductDetailsErrorState());
+      }
+    });
+  }
 
   final AppRepository repository;
 }
